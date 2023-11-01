072316072d80137e75b7fd740add9203d3dc55be
Samanway Ghatak <samanwayghatak@gmail.com>
2023-11-01 21:16:55 +0000

    Added basic rendering test cases.

--- src/components/NewItem.test.tsx
+++ src/components/NewItem.test.tsx
@@ -16,9 +16,59 @@ describe("NewItem Component", () => {
 
             const { getByRole } = within(formControl)
         })
-        it("should have a name textbox", () => {})
-        it("should have a description textbox", () => {})
-        it("should have a quantity textbox", () => {})
+        it("should have a name textbox", () => {
+            render(<NewItem />)
+            let formControl = screen.getByRole('form', {
+                name: "Enter item details"
+            })
+            const {getByRole} = within(formControl);
+
+            let nameTextbox = getByRole('textbox', {
+                name: 'Item Name:'
+            })
+
+            expect(nameTextbox).toBeInTheDocument();
+        })
+        it("should have a description textbox", () => {
+            render(<NewItem />)
+            let formControl = screen.getByRole('form', {
+                name: "Enter item details"
+            })
+            const {getByRole} = within(formControl);
+
+            let descTextbox = getByRole('textbox', {
+                name: 'Description (Optional):'
+            })
+
+            expect(descTextbox).toBeInTheDocument();
+        })
+        it("should have a quantity number input", () => {
+            render(<NewItem />)
+            let formControl = screen.getByRole('form', {
+                name: "Enter item details"
+            })
+            const {getByRole} = within(formControl);
+
+            let quantityTextBox = getByRole('spinbutton', {
+                name: 'Quantity (Optional):'
+            })
+
+            expect(quantityTextBox).toBeInTheDocument();
+        })
+        it("should have Reset and Add Item Button", () => {
+            render(<NewItem />)
+
+            let addItemButton = screen.getByRole('button', {
+                name: 'Add Item'
+            })
+
+            let resetButton = screen.getByRole('button', {
+                name: 'Add Item'
+            })
+
+            expect(addItemButton).toBeInTheDocument();
+            expect(resetButton).toBeInTheDocument();
+        })
     })
     describe("Behavior tests - ", () => {
         
--- src/components/NewItem.tsx
+++ src/components/NewItem.tsx
@@ -5,7 +5,14 @@ const NewItem: React.FC = (props) => {
         <h1>Create new item</h1>
         <label id='form-label' htmlFor='new-item-form'>Enter item details</label>
         <form id='new-item-form' aria-labelledby='form-label'>
+            <label htmlFor='item-name'>Item Name:</label>
             <input type='text' id="item-name" name="item-name"></input>
+            <label htmlFor='item-description'>Description (Optional):</label>
+            <textarea id='item-description'></textarea>
+            <label htmlFor='item-quantity'>Quantity (Optional):</label>
+            <input id='item-quantity' type='number' min={0} step={1} />
+            <button>Add Item</button>
+            <button>Reset</button>
         </form>
     </>
 }
